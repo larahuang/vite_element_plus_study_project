fc650cd9926dd25035d2bef2dca81463e10a7ba6
lara huang <lara1105huang@gmail.com>
2024-05-26 22:47:36 +0800

    列印設定筆數

--- src/views/Table2.vue
+++ src/views/Table2.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="checkWork_area"> 
     <div class="search_bar_group">
+         <el-button type="primary" @click="openPrint = true">列印</el-button>
         <el-button type="primary"> 導出Excel</el-button>
         <!--綁定input  v-model="searchQuery"-->
         <el-input 
@@ -8,7 +9,7 @@
         <i class="icon-search"></i>
     </div>
     <!----table-->
-<div class="check_table_area">
+<div class="check_table_area" id="printArea">
     <table class="table">
         <tr>
              <span @click="sortededItems">1</span>
@@ -45,6 +46,22 @@
             @sendOnChange="changeItemsPerPage"
             />
 
+             <Teleport  to="body">
+                <div v-if="openPrint" class="modal">
+                    <div class="modal-main">
+                        <div class="modal-header">
+                            <a @click="openPrint = false">
+                                <i class="icon-cross"></i>
+                            </a>
+                        </div >
+                        <div class="modal-body">
+                         列印   <input v-model="printItem" @change="getPrintItem(printItem)"/>筆
+                        </div>
+                         <el-button type="primary" v-print="print">列印</el-button>
+                    </div>
+                </div>
+             </Teleport>
+
 </div>
 </template>
 
@@ -54,6 +71,13 @@ import { ref, onMounted, computed, watch, watchEffect } from 'vue';
 import Pagination from '@/components/Pagination.vue';
 import { useRouter } from 'vue-router';
 const router = useRouter()
+
+const openPrint = ref<boolean>(false);
+const printItem= ref<number |any>(null);
+const getPrintItem = (printItem: number) => {
+    console.log('獲取到',printItem)
+   changeItemsPerPage(printItem)
+}
 // 搜尋表單綁定
 const searchQuery = ref<string>('');
 // 當前頁面
@@ -64,6 +88,11 @@ const changeItemsPerPage = (event: number) => {
     itemsPerPage.value = event;
 }
 const pagePerOptions = [
+     { value: '1', label: '1筆' },
+    { value: '2', label: '2筆' },
+    { value: '3', label: '4筆' },
+    { value: '5', label: '5筆' },
+    { value: '6', label: '6筆' },
     { value: '10', label: '10筆' },
     { value: '15', label: '15筆' },
     { value: '20', label: '20筆' },
@@ -192,10 +221,88 @@ const getItems = async () => {
         console.log('err', err);
     }
 }
+
+interface printType{
+  url?: string |undefined,
+  id: string |undefined,
+  popTitle: string |undefined,
+  extraHead: string |undefined,
+  preview: boolean, 
+  previewTitle: string |undefined,
+  previewPrintBtnLabel: string |undefined,
+  zIndex:number |null,
+  previewBeforeOpenCallback(): void;
+  previewOpenCallback(): void;
+  beforeOpenCallback(): void;
+  openCallback(): void;
+  closeCallback(): void;
+  clickMounted(): void;
+ // asyncUrl(reslove:any, vue:any): void;
+  standard: string |undefined,
+  extarCss: string |undefined,
+}
+const msg = ref<string>('Welcome to Your Vue.js App');
+const print =ref<printType>(
+    {
+      //  url: 'http://localhost:8080/',
+        // asyncUrl (reslove:any, vue:any) {
+
+        //         // setTimeout(() => {
+        //         //   reslove('http://localhost:8080/')
+        //         // }, 2000)
+        // },
+        id: 'printArea',
+        popTitle: '配置页眉标题', // 打印配置页上方的标题
+         extraHead: '<meta http-equiv="Content-Language"content="zh-tw"/>', // 最上方的头部文字，附加在head标签上的额外标签，使用逗号分割
+        preview: true, // 是否启动预览模式，默认是false
+        previewTitle: '員工設定列表', // 打印预览的标题
+        previewPrintBtnLabel: '預覽後，開始打印', // 打印预览的标题下方的按钮文本，点击可进入打印
+        zIndex: 20002, // 预览窗口的z-index，默认是20002，最好比默认值更高
+        previewBeforeOpenCallback () { console.log('正在加载预览窗口！'); console.log(msg.value, this) }, // 预览窗口打开之前的callback
+        previewOpenCallback () { console.log('已经加载完预览窗口，预览打开了！') }, // 预览窗口打开时的callback
+        beforeOpenCallback () { console.log('开始打印之前！') }, // 开始打印之前的callback
+        openCallback () { console.log('执行打印了！') }, // 调用打印时的callback
+        closeCallback () { console.log('关闭了打印工具！') }, // 关闭打印的callback(无法区分确认or取消)
+        clickMounted () { console.log('点击v-print绑定的按钮了！') },
+        standard: '',
+        extarCss: '',
+        
+    }
+)
 onMounted(() => {
     getItems();
 })
 </script>
+
+<style lang="scss">
+.previewBodyUtil{
+ background: #ffff !important;
+   
+.previewBodyUtilPrintBtn{
+    display: block;
+    text-align: center;
+    line-height: 30px;
+    width: 150px!important;
+    height: 30px!important;
+    border-radius: 8px!important;
+    padding:8px 14px;
+    background: cornflowerblue !important;
+    &:hover{
+         background: rgb(54, 79, 125) !important;
+    }
+}
+}
+@media print {
+/*最外层打印节点*/
+  #printArea {
+    display:block;
+  //  height: auto;
+    overflow: hidden;
+    margin:0;
+    height: 266.5mm;//采用默认页眉页脚时，单页内容大概长度，多页时倍数乘
+  }
+}
+</style>
 
 
 
